cf4792d9359957b061cd25ed622a2e0e8d280203
NyiKoTwy <nyikotwydilonay@gmail.com>
2025-03-16 00:10:22 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -222,10 +222,10 @@ app.post("/login", async (req, res) => {
         if (user.password === password) {
 		    const token = generateToken(user);
 		    res.cookie("access-token", token, { 
-                maxAge: 60, 
+                maxAge: 900000, 
                 httpOnly: true, 
                 secure: isProduction, // Secure cookies only in production
-                sameSite: isProduction ? "None" : "Lax"
+                sameSite: isProduction ? "None" : "Lax" 
             });
             res.json({ message: "Login successful", redirect: "/dashboard" });
         } else {
